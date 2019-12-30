e420f33ae0fc5ef86968f86ec0a65b15e5432997
danWagar <dandwagar@gmail.com>
2019-12-30 07:56:20 -0800

    update seeds and related for words table

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -21,14 +21,16 @@ VALUES
 
 INSERT INTO "word" ("id", "language_id", "original", "translation", "next")
 VALUES
-  (1, 1, 'entraine toi', 'practice', 2),
-  (2, 1, 'bonjour', 'hello', 3),
-  (3, 1, 'maison', 'house', 4),
-  (4, 1, 'développeur', 'developer', 5),
-  (5, 1, 'traduire', 'translate', 6),
-  (6, 1, 'incroyable', 'amazing', 7),
-  (7, 1, 'chien', 'dog', 8),
-  (8, 1, 'chat', 'cat', null);
+  (1, 1, 'salle de repos', 'restroom', 2),
+  (2, 1, 'l''eau', 'water', 3),
+  (3, 1, 's''il vous plaît', 'please', 4),
+  (4, 1, 'bonjour', 'hello', 5),
+  (5, 1, 'merci', 'thank you', 6),
+  (6, 1, 'pardon', 'sorry', 7),
+  (7, 1, 'café', 'coffee', 8),
+  (8, 1, 'au revoir', 'goodbye', 9),
+  (9, 1, 'bonne nuit', 'good night', 10),
+  (10, 1, 'je cherche', 'I''m looking for', null);
 
 UPDATE "language" SET head = 1 WHERE id = 1;
 
